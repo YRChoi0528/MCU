3762827f9b6c97fdba1776a6593626b074c54e0e
YRChoi0528 <cyr4981@gmail.com>
2026-02-03 20:52:19 +0900

    Add files via upload

---
+++ 00_Basic/04_Structure.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "04_structure.h"
+
+int sum(S s)
+{
+    /*
+    s.a : s 의 멤버 a
+    s.b : s 의 멤버 b
+    */
+    return s.a + s.b;
+}
+
+void inc(S *p)
+{
+    p->a++;
+    p->b++;
+}
+
+int main(void)
+{
+    S s = {1, 2};
+    int x;
+
+    x = sum(s);
+    printf("x=%d\n", x);
+
+    inc(&s);
+    printf("s.a=%d s.b=%d\n", s.a, s.b);
+
+    {
+        S t[3] = {{1,2},{3,4},{5,6}};
+        for (int i = 0; i < 3; i++)
+            printf("%d\n", sum(t[i]));
+    }
+
+    printf("s의 주소: %p\n", &s);
+    printf("s.a의 주소: %p\n", &s.a);
+    printf("s.b의 주소: %p\n", &s.b);
+
+    return 0;
+}
