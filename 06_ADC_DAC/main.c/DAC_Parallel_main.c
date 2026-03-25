273f643906bce53bd13625d2e5b4606ba621c651
YRChoi0528 <cyr4981@gmail.com>
2026-03-25 22:18:33 +0900

    Refactor DAC functions and add TIM initialization
    Refactor DAC function signatures and add timer initialization and delay functions.

--- 06_ADC_DAC/main.c/DAC_Parallel_main.c
+++ 06_ADC_DAC/main.c/DAC_Parallel_main.c
@@ -1,5 +1,4 @@
 #include "stm32f10x_lib.h"
-#include "user_delay.h"
 #include "System_func.h"
 
 /* 
@@ -26,14 +25,16 @@
 
 /* 함수 선언 */
 void DAC_init();
-void DAC_vol_gen(unsigned int selAB,unsigned char voltage);
+void DAC_vol_gen(u16 selAB, u8 voltage);
+void TIM_init();
+void delay_ms(u16 time);
 
 int main(void){
   Init_STM32F103();
-  Timer3_Delay_init();
+  TIM_init();
 
   /* 출력 전압 변수 초기화 (VolA: 증가, VolB: 감소) */
-  unsigned char VolA=0x01,VolB=0xFF;
+  u8 VolA=0x01,VolB=0xFF;
 
   /* 병렬 DAC 제어용 GPIO 포트 초기화 */
   DAC_init();
@@ -112,4 +113,26 @@ void DAC_vol_gen(unsigned int selAB,unsigned char voltage){
    */
   GPIO_SetBits(AD7302_CTRL,P_WR);
   GPIO_SetBits(AD7302_CTRL,P_CS);
+}
+
+void TIM_init(){
+  TIM_TimeBaseInitTypeDef TIM3_TimeBaseInitStruct;
+  
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3,ENABLE);
+ 
+  TIM3_TimeBaseInitStruct.TIM_Prescaler = 7200 -1;  
+  TIM3_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM3_TimeBaseInitStruct.TIM_Period = 10-1;  
+  TIM3_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM_TimeBaseInit(TIM3,&TIM3_TimeBaseInitStruct);
+}
+
+void delay_ms(u16 time){
+  TIM_Cmd(TIM3,ENABLE);
+
+  while(--time){
+     while(TIM_GetFlagStatus(TIM3,TIM_IT_Update)==RESET);
+     TIM_ClearFlag(TIM3,TIM_FLAG_Update);
+  }
+  TIM_Cmd(TIM3,DISABLE);
 }
