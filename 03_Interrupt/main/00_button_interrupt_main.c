cf7a2774369e48f794ae52b4e49f884a51700c6f
YRChoi0528 <cyr4981@gmail.com>
2026-03-25 21:01:47 +0900

    Refactor GPIO and interrupt initialization
    Refactor interrupt initialization and GPIO setup for button inputs and LED outputs. Consolidate EXTI configuration for multiple pins and remove unnecessary delay logic.

--- 03_Interrupt/main/00_button_interrupt_main.c
+++ 03_Interrupt/main/00_button_interrupt_main.c
@@ -1,70 +1,80 @@
 #include "stm32f10x_lib.h"
 #include "System_func.h"
 
-void Timer_init(){
-  TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
-  
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);
- 
-  TIM_TimeBaseInitStruct.TIM_Prescaler = 7200-1;  
-  TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
-  TIM_TimeBaseInitStruct.TIM_Period = 10-1;  
-  TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
-  TIM_TimeBaseInit(TIM2,&TIM_TimeBaseInitStruct);
-}
-
-void delay_ms(u16 time){
-  u16 i; 
-  TIM_Cmd(TIM2,ENABLE); /* 타이머 시작 */    
-  for(i=0;i<time;i++){
-    while(TIM_GetFlagStatus(TIM2,TIM_IT_Update)==RESET);
-    TIM_ClearFlag(TIM2,TIM_FLAG_Update);
-  }
-  TIM_Cmd(TIM2,DISABLE); /* 타이머 정지 */
-}
-
-/* 
+/*
  * 전역 변수 설정
- * stop: LED 이동 정지/재개 플래그 (0: 이동, 1: 정지)
- * dir: LED 이동 방향 플래그 (0: 왼쪽 시프트, 1: 오른쪽 시프트)
+ * LED_data: 현재 GPIOC로 출력할 LED 데이터
+ * vu16은 volatile unsigned 16-bit 정수형이며, Library → inc → stm32f10x_type.h 에서 확인 가능하다.
+ * volatile은 컴파일러가 해당 변수를 최적화로 제거하거나
+ * 레지스터에만 보관하지 않도록 하기 위해 사용한다.
+ * 즉, 외부 요인(인터럽트 등)에 의해 값이 바뀔 수 있음을 의미한다.
  */
-char stop=0, dir=0;
+vu16 LED_data=0x0000; 
+
+void INT_init();
 
 int main(void){
-  EXTI_InitTypeDef EXTI_InitStructure;
-  NVIC_InitTypeDef NVIC_InitStructure;  
   GPIO_InitTypeDef GPIO_InitStructure;
-  
-  /* 초기 점등할 LED 위치 (0x0080 = 최상위 비트(PC7) ) */
-  unsigned int LED_data = 0x0080;
 
   Init_STM32F103();
-  Timer_init();
-  
-  /* 
+
+  /*
    * 클럭 활성화 (APB2)
-   * GPIOB: 버튼 입력용
+   * GPIOB: 스위치 입력용
    * GPIOC: LED 출력용
-   * AFIO: 외부 인터럽트(EXTI) 라인과 포트 연결 설정용 (필수)
+   * AFIO : EXTI 라인과 GPIO 포트 연결 설정용
    */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC
-                         | RCC_APB2Periph_AFIO, ENABLE);
-  
-  /* PC0~PC7 (LED 연결 핀) Push-Pull 출력 설정 */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | 
+                         RCC_APB2Periph_AFIO, ENABLE);
+
+  /*
+   * GPIOC 하위 8비트(PC0~PC7)를 LED 출력용 Push-Pull 출력으로 설정
+   * 0xFF는 PC0~PC7에 해당하는 비트 마스크이다.
+   */
+  GPIO_InitStructure.GPIO_Pin = 0xFF;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_Init(GPIOC, &GPIO_InitStructure);
-  
-  /* PB0~PB2 (버튼 연결 핀) Floating 입력 설정 */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2;
+
+  /*
+   * GPIOB의 PB0, PB1, PB2, PB5, PB6, PB7, PB8, PB9를
+   * 스위치 입력용 Floating 입력으로 설정
+   */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_5|
+                                GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
   GPIO_Init(GPIOB, &GPIO_InitStructure);
   
+  INT_init();
+  
+  while (1){
+    /*
+     * 현재 LED_data 값을 그대로 GPIOC 출력 레지스터에 기록
+     * LED 패턴은 인터럽트 서비스 루틴에서 변경된다고 가정
+     */
+    GPIO_Write(GPIOC, LED_data);
+  }//end while
+}//end main
+
+/* =============== Function =============== */
+
+/*
+ * 외부 인터럽트 초기화 함수
+ * 대상 입력 핀:
+ * - EXTI0   → PB0
+ * - EXTI1   → PB1
+ * - EXTI2   → PB2
+ * - EXTI5~9 → PB5, PB6, PB7, PB8, PB9
+ * Rising Edge에서 인터럽트가 발생하도록 설정한다.
+ */
+void INT_init(){
+  EXTI_InitTypeDef EXTI_InitStructure;
+  NVIC_InitTypeDef NVIC_InitStructure;
+
   /*
-   * NVIC(중첩 인터럽트 제어기) 설정
-   * EXTI0, EXTI1, EXTI2 인터럽트 채널 활성화 및 우선순위 지정
-   * PreemptionPriority가 낮을수록 우선순위가 높음 (0이 가장 먼저 실행됨)
+   * NVIC 설정
+   * EXTI0, EXTI1, EXTI2, EXTI9_5 인터럽트 채널 활성화
+   * 숫자가 작을수록 더 높은 우선순위를 가진다.
    */
   NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQChannel;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
@@ -84,60 +94,34 @@ int main(void){
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
   
+  NVIC_InitStructure.NVIC_IRQChannel = EXTI9_5_IRQChannel;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
   /*
-   * AFIO 설정: GPIOB의 0, 1, 2번 핀을 각각 EXTI Line 0, 1, 2에 연결
+   * AFIO 설정
+   * GPIOB의 각 핀을 대응되는 EXTI 라인에 연결
    */
   GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource0);
   GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource1);
   GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource2);
-  
+  GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource5);
+  GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource6);
+  GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource7);
+  GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource8);
+  GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource9);
+
   /*
-   * EXTI(외부 인터럽트) 모드 및 트리거 설정
-   * 버튼이 눌릴 때(Rising Edge) 인터럽트가 발생하도록 설정
+   * EXTI 설정
+   * EXTI Line 0, 1, 2, 5, 6, 7, 8, 9를 인터럽트 모드로 사용
+   * 버튼 입력이 High가 되는 순간(Rising Edge) 인터럽트 발생
    */
-  EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+  EXTI_InitStructure.EXTI_Line = EXTI_Line0 | EXTI_Line1 | EXTI_Line2 | EXTI_Line5|
+                                 EXTI_Line6 | EXTI_Line7 | EXTI_Line8 | EXTI_Line9;
   EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
   EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
   EXTI_InitStructure.EXTI_LineCmd = ENABLE;
   EXTI_Init(&EXTI_InitStructure);
-  
-  EXTI_InitStructure.EXTI_Line = EXTI_Line1;
-  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
-  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-  EXTI_Init(&EXTI_InitStructure);
-  
-  EXTI_InitStructure.EXTI_Line = EXTI_Line2;
-  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
-  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-  EXTI_Init(&EXTI_InitStructure);
-  
-  while (1){
-    /* stop 상태가 0(진행)일 때만 LED 시프트 동작 수행 */
-    if(stop == 0){
-      if(dir==0){
-        /* dir == 0: 왼쪽으로 이동. 최하위 비트(0x01)면 다시 최상위(0x80)로 순환 */
-        if(LED_data==0x80)
-          LED_data = 0x01;
-        else
-          LED_data <<= 1;
-      }
-      else{
-        /* dir == 1: 오른쪽으로 이동. 최상위 비트(0x80)면 다시 최하위(0x01)로 순환 */
-        if(LED_data==0x01)
-          LED_data = 0x80;
-        else
-          LED_data >>= 1;
-      }
-      
-      /* 이전 LED 상태 지우고 새로운 패턴 출력 후 125ms 대기 */
-      GPIO_ResetBits(GPIOC, 0xFF);
-      GPIO_SetBits(GPIOC, LED_data);
-      delay_ms(125);
-    }
-    else{
-      /* stop == 1 이면 아무 동작 없이 무한 대기 (LED 멈춤 효과) */
-    }
-  }//end while
-}//end main
+}
